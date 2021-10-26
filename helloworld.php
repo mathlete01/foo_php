babca2dcd75c1fb3b4fcd0a1387baf87bcda1dce
mathlete01 <msallin@gmail.com>
2021-10-26 12:56:02 -0700

    Change Dad to Butcher

--- helloworld.php
+++ helloworld.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Your Mom</title>
+    <title>Your Butcher</title>
   </head>
   <body>
     <?php
-      $foo = " your Mom "
+      $foo = " Your Butcher "
     ?>
-    <?php echo "Hello" . " Mom!"; ?><br>
+    <?php echo "Hello" . " Butcher!"; ?><br>
     <?php echo 2 + 3; ?><br>
     Check it: <?php echo $foo; ?><br>
     Check it again: <?php echo strtoupper($foo); ?><br>
