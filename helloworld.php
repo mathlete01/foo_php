f36cd6bb40bfe75b1170cb495b8b4e50ce6750ac
mathlete01 <msallin@gmail.com>
2021-10-26 12:49:09 -0700

    Remove .DS_Store

--- helloworld.php
+++ helloworld.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Your Mom</title>
+    <title>Your Mother</title>
   </head>
   <body>
     <?php
-      $foo = " your mom "
+      $foo = " your mother "
     ?>
-    <?php echo "Hello" . " Mom!"; ?><br>
+    <?php echo "Hello" . " Mother!"; ?><br>
     <?php echo 2 + 3; ?><br>
     Check it: <?php echo $foo; ?><br>
     Check it again: <?php echo strtoupper($foo); ?><br>
