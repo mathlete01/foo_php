ade46a741b144ec6eb5b086c34fed5eaf836a7a5
mathlete01 <msallin@gmail.com>
2021-10-26 12:36:32 -0700

    Add helloworld.php to the director

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Your Mom</title>
+  </head>
+  <body>
+    <?php
+      $foo = " your mom "
+    ?>
+    <?php echo "Hello" . " Mom!"; ?><br>
+    <?php echo 2 + 3; ?><br>
+    Check it: <?php echo $foo; ?><br>
+    Check it again: <?php echo strtoupper($foo); ?><br>
+    
+  </body>
+</html>
