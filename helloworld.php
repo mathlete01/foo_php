82d7c70e036afc3d27c07195c9c2b255e9d6a69b
mathlete01 <msallin@gmail.com>
2021-10-26 12:52:08 -0700

    Change Father to Dad

--- helloworld.php
+++ helloworld.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Your Father</title>
+    <title>Your Dad</title>
   </head>
   <body>
     <?php
-      $foo = " your Father "
+      $foo = " your Dad "
     ?>
-    <?php echo "Hello" . " Father!"; ?><br>
+    <?php echo "Hello" . " Dad!"; ?><br>
     <?php echo 2 + 3; ?><br>
     Check it: <?php echo $foo; ?><br>
     Check it again: <?php echo strtoupper($foo); ?><br>
